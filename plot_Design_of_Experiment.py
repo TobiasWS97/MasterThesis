70d5769a6375630467650115b8ad6ebcd20eb950
Tobias Widsteen Solberg <87205087+TobiasWS97@users.noreply.github.com>
2024-08-01 07:02:15 -0300

    Update plot_Design_of_Experiment.py

--- plot_Design_of_Experiment.py
+++ plot_Design_of_Experiment.py
@@ -7,7 +7,6 @@ plt.style.use('seaborn-v0_8-darkgrid')
 plt.rcParams['figure.figsize'] = [10, 6]  # Set a consistent figure size
 
 
-# Replace 'your_data_file.csv' with the path to your actual CSV file
 file_path = r'C:\Users\Bruker\OneDrive - NTNU\Y5\Master_thesis\Idun\Design_of_Experiment\LHC_cat_1000.csv'
 
 # Read the CSV file into a DataFrame
@@ -19,11 +18,10 @@ print("Data Head:\n", data.head())
 # Display the data types to ensure they are as expected
 print("\nData Types:\n", data.dtypes)
 
-# Assuming the CSV has columns named 'Temperature' and 'Class'
 temperature_column = 'Temperature'
 class_column = 'Class'
 
-# Convert temperature to numeric if it's not already
+# Convert temperature to numeric 
 data[temperature_column] = pd.to_numeric(data[temperature_column], errors='coerce')
 
 # Convert class to string to map properly
@@ -45,13 +43,10 @@ print("\nMapped Concentration Column:\n", data['Concentration'].head())
 # Ensure concentration is a categorical type with the right order
 data['Concentration'] = pd.Categorical(data['Concentration'], categories=['100%', '75%', '66%'], ordered=True)
 
-# Display final data to be plotted
-print("\nFinal Data:\n", data.head())
 
 # Plot using seaborn
 plt.figure(figsize=(10, 6))
 sns.swarmplot(x='Concentration', y=temperature_column, data=data, palette={'100%': 'red', '75%': 'green', '66%': 'blue'})
-#plt.title('Temperature vs. Li Concentration')
 plt.xlabel('Li Concentration')
 plt.ylabel('Temperature (K)')
 
